c63910beabcc3863cecc05c9ffdad9a72ba17a09
pavanyalala <pavanyalala4508@gmail.com>
2021-08-18 10:10:31 +0530

    [pavan]:Gambler start game added

--- GamblerSimulation/GamblerStak.cs
+++ GamblerSimulation/GamblerStak.cs
@@ -10,12 +10,7 @@ namespace GamblerSimulation
         {
             const int START = 100;
             const int BET = 1;
-            int game = 0;
-            for (int i = BET;i<=START;i++)
-            {
-                game++;
-                Console.WriteLine("Number of Games : " + game);
-            }
+            
         }
         
     }
