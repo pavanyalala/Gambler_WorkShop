6806246326068678aefdc37d1a6558fce7e8151d
pavanyalala <pavanyalala4508@gmail.com>
2021-08-18 09:34:09 +0530

    [pavan]:Gambler simulation game initialized

---
+++ GamblerSimulation/GamblerStak.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerSimulation
+{
+    class GamblerStak
+    {
+        public void Game()
+        {
+            const int START = 100;
+            const int BET = 1;
+            int game = 0;
+            for (int i = BET;i<=START;i++)
+            {
+                game++;
+                Console.WriteLine("Number of Games : " + game);
+            }
+        }
+        
+    }
+}
