8c69f530b96a2e4fb2dcd88b1358eec6dbff5d71
pavanyalala <pavanyalala4508@gmail.com>
2021-08-18 09:23:11 +0530

    [pavan]:Initial Message Added

---
+++ GamblerSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamblerSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Simulation program");
+        }
+    }
+}
